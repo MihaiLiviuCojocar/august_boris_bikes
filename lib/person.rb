9bfdf7a5e6e6d7c87583fefd7d2c5c8d1173eae4
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2015-04-07 21:20:47 +0100

    fixed indentation

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,22 @@
 class Person
+  def initialize(bike: nil)
+    @bike = bike
+  end
 
-	def initialize(bike: nil)
-		@bike = bike
-	end
+  def has_bike?
+    !@bike.nil?
+  end
 
-	def has_bike?
-		!@bike.nil?
-	end
+  def rent_bike_from station
+    @bike = station.release_a_bike
+  end
 
-	def rent_bike_from station
-		@bike = station.release_a_bike
-	end
+  def return_bike_to station
+    station.dock(@bike)
+    @bike = nil
+  end
 
-	def return_bike_to station
-		station.dock(@bike)
-		@bike = nil
-	end
-
-	def have_accident!
-		@bike.break!
-	end
-
+  def have_accident!
+    @bike.break!
+  end
 end
