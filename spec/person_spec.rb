78644754e03e02e3d2a36a1685dc41ca1371ad84
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2015-04-07 21:18:46 +0100

    fixed indentation

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,52 +1,51 @@
 require 'person'
 
 describe Person do
+  let(:person)           { Person.new             }
+  let(:person_with_bike) { Person.new(bike: bike) }
+  let(:bike)             { double :bike           }
+  let(:old_street)       { double :station        }
 
-	let(:person)           { Person.new             }
-	let(:person_with_bike) { Person.new(bike: bike) }
-	let(:bike)             { double :bike           }
-	let(:old_street)       { double :station        }
+  it 'has no bike' do
+    expect(person.has_bike?).to be false
+  end
 
-	it 'has no bike' do
-		expect(person.has_bike?).to be false
-	end
+  it 'can have a bike' do
+   person = Person.new(:bike => :banana)
+   expect(person).to have_bike
+  end
 
-	it 'can have a bike' do
-		person = Person.new(:bike => :banana)
-		expect(person).to have_bike
-	end
+  it 'can rent a bike from a station' do
+    expect(old_street).to receive(:release_a_bike).and_return(bike)
 
-	it 'can rent a bike from a station' do
-		expect(old_street).to receive(:release_a_bike).and_return(bike)
+    person.rent_bike_from old_street	
+  end
 
-		person.rent_bike_from old_street	
-	end
+  it 'has a bike after renting one' do
+    allow(old_street).to receive(:release_a_bike).and_return(bike)
 
-	it 'has a bike after renting one' do
-		allow(old_street).to receive(:release_a_bike).and_return(bike)
+    person.rent_bike_from old_street	
 
-		person.rent_bike_from old_street	
+    expect(person).to have_bike		
+  end
 
-		expect(person).to have_bike		
-	end
+  it 'can return a bike to a station' do
+    expect(old_street).to receive(:dock).with(bike)
 
-	it 'can return a bike to a station' do
-		expect(old_street).to receive(:dock).with(bike)
+    person_with_bike.return_bike_to old_street
+  end
 
-		person_with_bike.return_bike_to old_street
-	end
+  it 'has no bike after returning it' do
+    allow(old_street).to receive(:dock).with(bike)	
 
-	it 'has no bike after returning it' do
-		allow(old_street).to receive(:dock).with(bike)	
+    person_with_bike.return_bike_to old_street
 
-		person_with_bike.return_bike_to old_street
+    expect(person_with_bike).not_to have_bike
+  end
 
-		expect(person_with_bike).not_to have_bike
-	end
+  it 'can break a bike' do
+    expect(bike).to receive(:break!)
 
-	it 'can break a bike' do
-		expect(bike).to receive(:break!)
-
-		person_with_bike.have_accident!
-	end
+    person_with_bike.have_accident!
+  end
 end
